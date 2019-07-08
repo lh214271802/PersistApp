079b0aa712e9b11a269715ab75e973bb56165f0a
lh214271802 <214271802@qq.com>
2019-07-08 23:24:14 +0800

    java鸭

---
+++ LibCommonRes/src/main/java/com/lightheart/base/BaseActivity.java
@@ -0,0 +1,53 @@
+package com.lightheart.base;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * @author liaohui
+ * @date 2019/7/8
+ */
+public class BaseActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
+}
---
+++ LibCommonRes/src/main/java/com/lightheart/base/BaseFragment.java
@@ -0,0 +1,8 @@
+package com.lightheart.base;
+
+/**
+ * @author liaohui
+ * @date 2019/7/8
+ */
+public class BaseFragment {
+}
